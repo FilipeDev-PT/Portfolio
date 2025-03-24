830452fc2fb03550e431a70b2aec71b5f0aaf73a
FilipeDev-PT <filipe.dev.contact@gmail.com>
2025-03-24 12:43:12 -0300

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/about/about";
+import Skills from "../pages/skills/skills";
+import Project from "../pages/project/project";
+
+const routesApp = () => {
+  const BackdropAction = ({ children }) => {
+    return children;
+  };
+
+  return (
+    <BackdropAction>
+      <Routes>
+        <Route path="/Postifolio" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
+    </BackdropAction>
+  );
+};
+
+export default routesApp;
